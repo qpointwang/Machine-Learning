7be1e443338fc2241f7b31ad737a46b80af2b5b4
qpointwang <qpointwang@163.com>
2019-04-20 09:55:46 -0400

    delete

--- SVM-Handwritten-digit-recognition/SVM_sklearn.py
+++ SVM-Handwritten-digit-recognition/SVM_sklearn.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 import scipy.io as sio
@@ -6,15 +9,18 @@ import logging
 # 数据集
 dataSet = sio.loadmat('mnist_train.mat')['mnist_train']
 lables = sio.loadmat('mnist_train_labels.mat')['mnist_train_labels']
+print(len(dataSet))
 dataSet = dataSet/255.0
 
+logging.info("Loading Files...")
 train_data = dataSet[0:50000]
 train_data_lables = lables[0:50000]
 test_data = dataSet[50000:60000]
 test_data_lables = lables[50000:60000]
-
+logging.info("Traning...")
 clf = svm.SVC(C=1.0, decision_function_shape='ovr')
 clf.fit(train_data, train_data_lables[:,0])
+logging.info("Training End")
 result = clf.predict(test_data)
 logging.basicConfig(level=logging.DEBUG,
                 format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
